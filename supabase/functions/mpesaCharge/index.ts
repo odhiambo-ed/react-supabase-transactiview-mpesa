8d81c8a0c0d47b8fc025316bcd83d70837e2ec26
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-23 12:21:10 +0300

    edit function

--- supabase/functions/mpesaCharge/index.ts
+++ supabase/functions/mpesaCharge/index.ts
@@ -1,5 +1,5 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { createClient } from "npm:@supabase/supabase-js@2.39.4";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.4";
 import crypto from "node:crypto";
 import { Buffer } from "node:buffer";
 
