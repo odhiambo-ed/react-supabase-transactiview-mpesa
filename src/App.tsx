ee8faa1b3a9bef970a14064f325339c1b9d049be
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-26 17:03:11 +0300

    add payment files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/SideBar";
+import TopNavbar from "./components/TopNavbar";
+import Dashboard from "./components/Dashboard";
+import HomePage from "./pages/HomePage";
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-      <p>Jesus I love You</p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="d-flex">
+        <Sidebar />
+        <div className="flex-grow-1">
+          <TopNavbar />
+          <div className="p-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/payment" element={<Payment />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
