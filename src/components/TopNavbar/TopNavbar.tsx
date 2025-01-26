1bbf4b59852aec43a549b96e60b1b7ba68ee3144
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-26 17:16:02 +0300

    add topnav

--- src/components/TopNavbar/TopNavbar.tsx
+++ src/components/TopNavbar/TopNavbar.tsx
@@ -1,9 +1,21 @@
-import './TopNavbar.css'
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import { RiSecurePaymentLine } from "react-icons/ri";
 
-const TopNavbar = () => {
+const TopNavbar: React.FC = () => {
   return (
-    <div>TopNavbar</div>
-  )
-}
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home" className="">
+          Code
+          <span>
+            <RiSecurePaymentLine />
+          </span>
+          Cafe
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;
