e46c892bc9038aed8ccd2a3b247332a4719204a4
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-28 10:28:59 +0300

    add login page to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import HomePage from "./pages/HomePage/HomePage";
 import Payment from "./pages/Payment/Payment";
 import Sidebar from "./components/Sidebar/Sidebar";
 import TopNavbar from "./components/TopNavbar/TopNavbar";
+import LoginPage from "./pages/Login/LoginPage";
+import { useAuth } from "./contexts/AuthContext";
 
 const App: React.FC = () => {
+  const { user } = useAuth();
+
   return (
     <BrowserRouter>
-      <div className="d-flex">
-        <Sidebar />
-        <div className="flex-grow-1">
-          <TopNavbar />
-          <div className="p-4">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/payment" element={<Payment />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="*"
+          element={user ? <MainAppLayout /> : <Navigate to="/login" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const MainAppLayout: React.FC = () => {
+  return (
+    <div className="d-flex">
+      <Sidebar />
+      <div className="flex-grow-1">
+        <TopNavbar />
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
