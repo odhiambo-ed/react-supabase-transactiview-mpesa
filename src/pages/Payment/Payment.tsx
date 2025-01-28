bfe219b8db2711f3dee84a4a4e3948213b7e7a93
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-28 20:13:07 +0300

    add mpesa logo image

--- src/pages/Payment/Payment.tsx
+++ src/pages/Payment/Payment.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import PaymentForm from "../../components/PaymentForm/PaymentForm";
+import Mpesa from "../../assets/images/M-PESA_LOGO.png";
 
 const Payment: React.FC = () => {
   return (
     <div className="p-4 w-60 border border-primary rounded">
-      <h1>Pay Now</h1>
+      <div className="row d-flex flex-row justify-content-around">
+        <h1>Pay Now</h1>
+        <img src={Mpesa} alt="Mpesa Logo" className="mpesa__logo" />
+      </div>
       <PaymentForm />
     </div>
   );
