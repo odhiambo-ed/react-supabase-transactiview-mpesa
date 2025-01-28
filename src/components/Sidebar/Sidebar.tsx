0673c0ddbd7b8de1f15697f7fed7c34714032d98
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-28 10:55:31 +0300

    add dashboard and logout to sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
-// src/components/Sidebar.tsx
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './Sidebar.css'
+import { useAuth } from "../../contexts/AuthContext";
+import "./Sidebar.css";
 
 const Sidebar: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
     <div className="d-flex flex-column vh-100 bg-dark text-white">
       <Nav className="flex-column p-3">
@@ -17,11 +19,19 @@ const Sidebar: React.FC = () => {
         <Nav.Link as={Link} to="/payment" className="text-white">
           Pay
         </Nav.Link>
+        <Nav.Link as={Link} to="/dashboard" className="text-white">
+          Dashboard
+        </Nav.Link>
       </Nav>
       <div className="mt-auto p-3">
         <Nav.Link as={Link} to="/payment">
           <button className="btn btn-primary w-100">Pay Now</button>
         </Nav.Link>
+      </div>
+      <div className="mt-auto p-3">
+        <button className="btn btn-danger w-100" onClick={signOut}>
+          Logout
+        </button>
       </div>
     </div>
   );
