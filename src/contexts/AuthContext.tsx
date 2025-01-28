2e5c79efa046343cb7197fd2f9ab03fc63c56b2d
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-28 18:02:56 +0300

    escape eslint

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -12,6 +12,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Initialize Supabase client
+/* eslint-disable */
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 interface AuthContextProps {
